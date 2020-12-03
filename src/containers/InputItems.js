a1783f1129881af24ce5e3588fb2f34429246040
lars <lars.andren@defa.com>
2020-12-03 09:02:48 +0100

    Final form

--- src/containers/InputItems.js
+++ src/containers/InputItems.js
@@ -5,10 +5,10 @@ import "./InputItems.css";
 import { useHistory } from "react-router-dom";
 
 export function encode(data: any) {
-    return Object.keys(data)
+  return Object.keys(data)
       .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
       .join("&");
-  }
+}
 
 export default function InputItems() {
   const history = useHistory();
@@ -25,14 +25,14 @@ export default function InputItems() {
     event.preventDefault();
 
     fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({
-          "form-name": "number-of-submissions",
-          "text-to-send": `${Math.random() + "message"}`,
-          ...formState
-        }),
-      });
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "number-of-submissions",
+        "text-to-send": `${Math.random() + "message"}`,
+        ...formState
+      }),
+    });
 
     history.push("thank-you");
   }
@@ -45,87 +45,87 @@ export default function InputItems() {
   const { comment, country } = formState;
 
   return (
-    <div className="container InputItems">
-      <div className="section">
-        <h1 className="title">Please answer the following questions.</h1>
-        <p className="subtitle">All responses are anonymous.</p>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group size="lg" controlId="country" className="field">
-            <Form.Label className="label">Select your country:</Form.Label>
-            <div className="control">
-              <label className="radio">
-                <input
-                  name="country"
-                  type="radio"
-                  value="Norway"
-                  checked={country === "Norway"}
-                  onChange={onValueChange}
-                />{" "}
-                Norway
-              </label>
-            </div>
-            <div className="control">
-              <label className="radio">
-                <input
-                  name="country"
-                  type="radio"
-                  value="Sweden"
-                  checked={country === "Sweden"}
-                  onChange={onValueChange}
-                />{" "}
-                Sweden
-              </label>
-            </div>
-            <div className="control">
-              <label className="radio">
-                <input
-                  name="country"
-                  type="radio"
-                  value="Other"
-                  checked={country === "Other"}
-                  onChange={onValueChange}
-                />{" "}
-                Other
-              </label>
-            </div>
-          </Form.Group>
+      <div className="container InputItems">
+        <div className="section">
+          <h1 className="title">Please answer the following questions.</h1>
+          <p className="subtitle">All responses are anonymous.</p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group size="lg" controlId="country" className="field">
+              <Form.Label className="label">Select your country:</Form.Label>
+              <div className="control">
+                <label className="radio">
+                  <input
+                      name="country"
+                      type="radio"
+                      value="Norway"
+                      checked={country === "Norway"}
+                      onChange={onValueChange}
+                  />{" "}
+                  Norway
+                </label>
+              </div>
+              <div className="control">
+                <label className="radio">
+                  <input
+                      name="country"
+                      type="radio"
+                      value="Sweden"
+                      checked={country === "Sweden"}
+                      onChange={onValueChange}
+                  />{" "}
+                  Sweden
+                </label>
+              </div>
+              <div className="control">
+                <label className="radio">
+                  <input
+                      name="country"
+                      type="radio"
+                      value="Other"
+                      checked={country === "Other"}
+                      onChange={onValueChange}
+                  />{" "}
+                  Other
+                </label>
+              </div>
+            </Form.Group>
 
-          <Form.Group size="lg" controlId="role" className="field">
-            <Form.Label className="label">Select your working environment (check all that apply):</Form.Label>
-            <div className="control">
-              <label className="checkbox">
-                <input
-                    name="role"
-                    type="checkbox"
-                    value="Outdoors"
-                />{" "}
-                Outdoors
-              </label>
-            </div>
-            <div className="control">
-              <label className="checkbox">
-                <input
-                    name="role"
-                    type="checkbox"
-                    value="Office"
-                />{" "}
-                Office
-              </label>
-            </div>
-            <div className="control">
-              <label className="checkbox">
-                <input
-                    name="role"
-                    type="checkbox"
-                    value="Home"
-                />{" "}
-                Home office
-              </label>
-            </div>
-          </Form.Group>
+            <Form.Group size="lg" controlId="role" className="field">
+              <Form.Label className="label">Select your working environment (check all that apply):</Form.Label>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="role"
+                      type="checkbox"
+                      value="Outdoors"
+                  />{" "}
+                  Outdoors
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="role"
+                      type="checkbox"
+                      value="Office"
+                  />{" "}
+                  Office
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="role"
+                      type="checkbox"
+                      value="Home"
+                  />{" "}
+                  Home office
+                </label>
+              </div>
+            </Form.Group>
 
-          <Form.Group size="lg" controlId="comment" className="field">
-            <Form.Label className="label">Have you during 2020 experienced physical discomfort caused by your work environment?</Form.Label>
+            <Form.Group size="lg" controlId="comment" className="field">
+              <Form.Label className="label">Is your home office lacking any vital tools or equipment?</Form.Label>
               <div className="control">
                 <label className="radio">
                   <input
@@ -156,29 +156,103 @@ export default function InputItems() {
                   Not sure
                 </label>
               </div>
-          </Form.Group>
+            </Form.Group>
 
-          <Form.Group size="lg" controlId="comment" className="field">
-            <Form.Label className="label">Additional comment</Form.Label>
-            <Form.Control
-              autoFocus
-              type="text"
-              name="comment"
-              value={comment}
-              onChange={onValueChange}
-            />
-          </Form.Group>
-          <Button
-            className="button is-link"
-            block
-            size="lg"
-            type="submit"
-            disabled={!validateForm()}
-          >
-            Submit
-          </Button>
-        </Form>
+            <Form.Group size="lg" controlId="tools" className="field">
+              <Form.Label className="label">What do you miss in your home office (check all that apply):</Form.Label>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="Desk"
+                  />{" "}
+                  Desk
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="Screen"
+                  />{" "}
+                  Large monitor
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="station"
+                  />{" "}
+                  Laptop docking station
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="chair"
+                  />{" "}
+                  Comfortable chair
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="coffee"
+                  />{" "}
+                  Coffee machine
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="drawers"
+                  />{" "}
+                  Drawers cabinet
+                </label>
+              </div>
+              <div className="control">
+                <label className="checkbox">
+                  <input
+                      name="tools"
+                      type="checkbox"
+                      value="notlisted"
+                  />{" "}
+                  Something not listed here
+                </label>
+              </div>
+            </Form.Group>
+
+            <Form.Group size="lg" controlId="comment" className="field">
+              <Form.Label className="label">Additional comment</Form.Label>
+              <Form.Control
+                  autoFocus
+                  type="text"
+                  name="comment"
+                  value={comment}
+                  onChange={onValueChange}
+              />
+            </Form.Group>
+            <Button
+                className="button is-link"
+                block
+                size="lg"
+                type="submit"
+                disabled={!validateForm()}
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
-    </div>
   );
 }
